717fa2208cfb7d0a6284dd969f87b74b6c32ceff
MPM Abeyrathne <pasindumaduwantha2019@gmail.com>
2023-07-20 04:21:24 +0530

    portfolio section updated

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,131 @@
 import React from "react";
 import "./portfolio.css";
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
 
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My React Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio_container">
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+
+          <div className="portfolio_item-cta">
+            <a href="https://github.com" className="btn" target="__blank">
+              Github
+            </a>
+            <a
+              href="https://dribbble.com/Alien_pixels"
+              className="btn btn-primary"
+              target="__blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG2} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com" className="btn" target="__blank">
+              Github
+            </a>
+            <a
+              href="https://dribbble.com/Alien_pixels"
+              className="btn btn-primary"
+              target="__blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG3} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com" className="btn" target="__blank">
+              Github
+            </a>
+            <a
+              href="https://dribbble.com/Alien_pixels"
+              className="btn btn-primary"
+              target="__blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG4} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com" className="btn" target="__blank">
+              Github
+            </a>
+            <a
+              href="https://dribbble.com/Alien_pixels"
+              className="btn btn-primary"
+              target="__blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG5} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com" className="btn" target="__blank">
+              Github
+            </a>
+            <a
+              href="https://dribbble.com/Alien_pixels"
+              className="btn btn-primary"
+              target="__blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG6} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com" className="btn" target="__blank">
+              Github
+            </a>
+            <a
+              href="https://dribbble.com/Alien_pixels"
+              className="btn btn-primary"
+              target="__blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
