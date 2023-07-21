bfac17b52a96bf23bca2bbf82445b626e64eec20
MPM Abeyrathne <pasindumaduwantha2019@gmail.com>
2023-07-22 02:36:53 +0530

    contact section updated

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,43 @@
 import React from "react";
 import "./contact.css";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact_container">
+        {/* Contact options */}
+
+        <div className="contact_options">
+          <article className="contact_option">
+            <MdOutlineMailOutline />
+            <h4>Email</h4>
+            <h5>pasindumaduwantha2019@gmail.com</h5>
+            <a href="mailto:pasindumaduwantha2019@gmail.com">Send a message</a>
+          </article>
+          <article className="contact_option">
+            <RiMessengerLine />
+            <h4>Messenger</h4>
+            <h5>Pasindu Maduwantha</h5>
+            <a href="https://m.me/pasindu.maduwantha.3150">Send a message</a>
+          </article>
+          <article className="contact_option">
+            <BsWhatsapp />
+            <h4>WhatsApp</h4>
+            <h5>+94788193729</h5>
+            <a href="https://api.whatsapp.com/send?phone=+94788193729">
+              Send a message
+            </a>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,13 +6,13 @@ import { FiDribbble } from "react-icons/fi";
 const HeaderSocials = () => {
   return (
     <div className="header_socials">
-      <a href="https://linkedin.com" target="_blank">
+      <a href="https://linkedin.com" target="__blank">
         <BsLinkedin />
       </a>
-      <a href="https://github.com" target="_blank">
+      <a href="https://github.com" target="__blank">
         <FaGithub />
       </a>
-      <a href="https://dribble.com.com" target="_blank">
+      <a href="https://dribble.com.com" target="__blank">
         <FiDribbble />
       </a>
     </div>
