a2db3e926ff658508b9ca43d6ee9fbc8e5a855b0
mpmabeyrathne <pasindumaduwantha2019@gmail.com>
2023-07-23 06:46:36 +0530

    contact section updated

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -18,23 +18,48 @@ const Contact = () => {
             <MdOutlineMailOutline />
             <h4>Email</h4>
             <h5>pasindumaduwantha2019@gmail.com</h5>
-            <a href="mailto:pasindumaduwantha2019@gmail.com">Send a message</a>
+            <a href="mailto:pasindumaduwantha2019@gmail.com" target="__blank">
+              Send a message
+            </a>
           </article>
           <article className="contact_option">
             <RiMessengerLine />
             <h4>Messenger</h4>
             <h5>Pasindu Maduwantha</h5>
-            <a href="https://m.me/pasindu.maduwantha.3150">Send a message</a>
+            <a href="https://m.me/pasindu.maduwantha.3150" target="__blank">
+              Send a message
+            </a>
           </article>
           <article className="contact_option">
             <BsWhatsapp />
             <h4>WhatsApp</h4>
             <h5>+94788193729</h5>
-            <a href="https://api.whatsapp.com/send?phone=+94788193729">
+            <a
+              href="https://api.whatsapp.com/send?phone+94788193729"
+              target="__blank"
+            >
               Send a message
             </a>
           </article>
         </div>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
   );
