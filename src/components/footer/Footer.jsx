9858e42255bfd3ef63f178871f48bff0cc8e7cf9
mpmabeyrathne <pasindumaduwantha2019@gmail.com>
2023-07-23 08:09:45 +0530

    Portfolio Completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer_logo">
+        PASINDU
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="https://facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="https://instagram.com">
+          <FaInstagram />
+        </a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy; Pasindu' Portfolio. ALL right reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
